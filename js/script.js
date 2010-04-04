8cab0213f148b13a3b9acc01cbb86669cccb31a3
Murilo Santana <mvrilo@gmail.com>
2010-04-03 22:00:31 -0300

    GET ip via freegeoip.net

--- js/script.js
+++ js/script.js
@@ -14,13 +14,24 @@ $(document).ready(function(){
 	$.ajax({
 		url : 'http://twitter.com/statuses/user_timeline/mvrilo.json?count=2&callback=?',
 		dataType : 'json',
-		success : function(json){
-			$('#twitter').attr('title',json[0].text);
-			$('#twitter').attr('alt',json[0].text);
+		success : function(data){
+			$('#twitter').attr('title',data[0].text);
+			$('#twitter').attr('alt',data[0].text);
 		},
 		error : function(){
 			$('#twitter').attr('title','Follow me on Twitter');
 			$('#twitter').attr('alt','Follow me on Twitter');
+		}
+	});
+
+	$.ajax({
+		url : 'http://freegeoip.net/json?callback=?',
+		dataType : 'jsonp',
+		success : function(data){
+			$('#ip').find('a').append(data.Ip);
+		},
+		error : function(){
+			$('#ip').find('a').append('could not obtain address');
 		}
 	});
 });
